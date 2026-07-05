c3d3ffdb69ddc778ddcde99ee0a1d891f0ea43e9
Ilya Pulcho <ilyapulcho@gmail.com>
2026-07-05 12:41:07 +0300

    feat: add news API tests with various search queries and error handling

--- tests/newsapi/data/news.data.ts
+++ tests/newsapi/data/news.data.ts
@@ -13,54 +13,64 @@ const formatDate = (date: Date) => date.toISOString().split('T')[0];
 
 export const newsTestData = [
   {
+    id: 1,
     q: 'Apple',
     from: formatDate(twoDaysAgo),
     sortBy: 'popularity',
   },
   {
+    id: 2,
     q: 'Apple',
     from: formatDate(yesterday),
     sortBy: 'popularity',
   },
-  {
+  { 
+    id: 3,
     q: 'Tesla',
     from: formatDate(yesterday),
     sortBy: 'publishedAt',
   },
-  {
+  {  
+    id: 4,
     q: 'AI',
     from: formatDate(yesterday),
     sortBy: 'relevancy',
   },
   {
+    id: 5,
     q: 'Google',
     from: formatDate(twoDaysAgo),
     sortBy: 'popularity',
   },
   {
+    id: 6,
     q: 'Amazon',
     from: formatDate(yesterday),
     sortBy: 'publishedAt',
   },
   {
+    id: 7,
     q: 'Nvidia',
     from: formatDate(yesterday),
     sortBy: 'relevancy',
   },
   {
-    q: 'Apple',
-    from: formatDate(tomorrow),
-    sortBy: 'popularity',
+  id: 8,
+  q: 'Microsoft',
+  from: formatDate(yesterday),
+  sortBy: 'popularity',
   },
   {
-    q: 'Microsoft',
-    from: formatDate(yesterday),
-    sortBy: 'popularity',
-  },
-  {
+    id: 9,
     q: 'Bitcoin',
     from: formatDate(yesterday),
     sortBy: 'publishedAt',
+  },
+  {
+    id: 10,
+    q: 'Meta',
+    from: formatDate(yesterday),
+    sortBy: 'popularity',
   },
 ];
 
---
+++ tests/newsapi/lesson14.fixture.news-param.spec.js
@@ -0,0 +1,28 @@
+import { expect } from '@playwright/test';
+import { test } from './helpers/fixtures';
+import { newsTestData } from './data/news.data';
+import { searchNewsResponseSchema } from '../../app/json-schemas/News';
+
+for (const { id, q, from, sortBy } of newsTestData) {
+  test(`${id}. Search "${q}" news`, async ({ newsRequest }) => {
+    const response = await newsRequest.get('everything', {
+      params: {
+        q,
+        from,
+        sortBy,
+      },
+      failOnStatusCode: true,
+    });
+
+    expect(response.status()).toBe(200);
+    expect(response.headers()['content-type']).toContain('application/json');
+
+    const body = await response.json();
+
+    searchNewsResponseSchema.parse(body);
+
+    expect(body.status).toBe('ok');
+    expect(body.totalResults).toBeGreaterThan(0);
+    expect(body.articles.length).toBeGreaterThan(0);
+  });
+}
